6446c7b2ed0b49060e9a3f92333c5d652853b55b
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-07-02 09:47:27 -0700

    Update simple_app.py

--- simple_app.py
+++ simple_app.py
@@ -21,13 +21,13 @@ with st.sidebar:
             st.markdown("**Don't have an API token?** Head over to [Replicate](https://replicate.com) to sign up for one.")
 
     os.environ['REPLICATE_API_TOKEN'] = replicate_api
+    
+    model = st.selectbox("Select a model",("meta/meta-llama-3-70b-instruct", "mistralai/mixtral-8x7b-instruct-v0.1", "google-deepmind/gemma-2b-it"), key="model")
+    if model == "google-deepmind/gemma-2b-it":
+        model = "google-deepmind/gemma-2b-it:dff94eaf770e1fc211e425a50b51baa8e4cac6c39ef074681f9e39d778773626"
     st.subheader("Adjust model parameters")
     temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.3, step=0.01)
     top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
-    model = st.selectbox("Select a model",("meta/meta-llama-3-70b-instruct", "mistralai/mixtral-8x7b-instruct-v0.1", "google-deepmind/gemma-2b-it"), key="model")
-
-    if model == "google-deepmind/gemma-2b-it":
-        model = "google-deepmind/gemma-2b-it:dff94eaf770e1fc211e425a50b51baa8e4cac6c39ef074681f9e39d778773626"
 
 # Store LLM-generated responses
 if "messages" not in st.session_state.keys():
